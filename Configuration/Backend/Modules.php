c1f3ce16cdb01e52def7aea5f430efb8947abfab
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2023-08-04 13:31:07 +0200

    [TASK] re-enable core web_layout and backend_layout

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -16,8 +16,6 @@ use Tvp\TemplaVoilaPlus\Controller\Backend\ControlCenter\Update\DataStructureV12
 use Tvp\TemplaVoilaPlus\Controller\Backend\ControlCenter\Update\DataStructureV13Controller;
 
 return [
-    'web_layout' => [
-    ],
     'web_TemplaVoilaPlusLayout' => [
         'parent' => 'web',
         'position' => ['top'],
